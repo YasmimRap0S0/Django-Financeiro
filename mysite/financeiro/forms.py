8bef3ae55be2d47d38db221ae069daad82fb8916
YasmimRaposo <yasmim.raposo@escolar.ifrn.edu.br>
2024-09-11 23:09:17 -0300

    ok

--- mysite/financeiro/forms.py
+++ mysite/financeiro/forms.py
@@ -6,7 +6,7 @@ class UsuarioForm(forms.ModelForm):
         model = Usuario
         fields = ['nome', 'email', 'senha']
         widgets = {
-            'nome': forms.TextInput(attrs={"placeholder": "Nome completo", "class": "rec-nome"}),
-            'email': forms.TextInput(attrs={"placeholder": "Email", "class":"RecEmail"}),
-            'senha': forms.PasswordInput(attrs={"placeholder": "Senha", "class":"RecSenha"})
+            'nome': forms.TextInput(attrs={"placeholder": "Nome completo", "class": "Rec-nome", "style": "width: 400px; height: 52.20px; left: 148px; top: 365px; position: absolute; background: white; border-radius: 50px; border: 1px #7B7B7B solid"}),
+            'email': forms.TextInput(attrs={"placeholder": "Email", "class":"RecEmail", "style": "width: 400px; height: 52.20px; left: 148px; top: 513px; position: absolute; background: white; border-radius: 50px; border: 1px #7B7B7B solid"}),
+            'senha': forms.PasswordInput(attrs={"placeholder": "Senha", "class":"RecSenha", "style": "width: 400px; height: 52.20px; left: 148px; top: 679px; position: absolute; background: white; border-radius: 50px; border: 1px #7B7B7B solid"})
         }
